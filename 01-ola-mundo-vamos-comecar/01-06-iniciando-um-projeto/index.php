c45269cdcec7e4e1b21e1d9341d96036edb1b309
Rodrigo Pereira <rodrigo55pereira@gmail.com>
2020-01-15 22:12:58 -0300

    Atualização

---
+++ 01-ola-mundo-vamos-comecar/01-06-iniciando-um-projeto/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Iniciando um projeto</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <?php
+        $start = "Vamos Começar!";
+        echo "<h1>Olá Mundo! {$start}</h1>";
+        echo "<p id='js'> Loading...</p>";
+    ?>
+<script src="assets/scripts.js"></script>    
+</body>
+</html>
